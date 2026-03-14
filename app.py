f45102d8fa08dca40fa20eb0635e4baf2a5a54c6
theredenemy <1234corbin.1234ad@gmail.com>
2026-03-14 00:18:36 -0700

    modified:   app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask import render_template
-from flask import render_template_string
 import os
 from makeConfig import makeConfig
 import configHelper
