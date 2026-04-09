f1fa160d24eed694db5f8951ffeb537f3d53917a
theredenemy <1234corbin.1234ad@gmail.com>
2026-04-08 19:09:44 -0700

    modified:   app.py

--- app.py
+++ app.py
@@ -39,7 +39,8 @@ def show_info():
     player = configHelper.read_config(config_file, "ORDINANCE", "player")
     timestamp = configHelper.read_config(config_file, "ORDINANCE", "timestamp")
     trigger = configHelper.read_config(config_file, "ORDINANCE", "trigger")
-    return jsonify({"player" : player, "timestamp" : timestamp, "trigger" : trigger}), 200
+    mode = configHelper.read_config(config_file, "ORDINANCE", "mode", default_value="game", is_int=False)
+    return jsonify({"player" : player, "timestamp" : timestamp, "trigger" : trigger, "mode" : mode}), 200
 @app.route("/ord/mode", methods=['GET', 'POST'])
 def set_mode():
     if request.method == 'GET':
