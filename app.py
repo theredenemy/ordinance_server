4213c3f9db2075be8b95615b1c4534dbd648f6e0
theredenemy <1234corbin.1234ad@gmail.com>
2026-05-17 19:37:37 -0700

    modified:   app.py

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ init_auth_db(auth_db)
 @app.errorhandler(404)
 def war_without_reason(e):
     # STOP TRYING BREAK
+    # this song is a banger
     return '<script>window.location.href="https://www.youtube.com/watch?v=Elj4zDLqJvw";</script>', 404
 @app.route("/")
 def main_page():
