cc804a4136348e5bd9f9c279629363ed9ed3718f
theredenemy <1234corbin.1234ad@gmail.com>
2026-05-20 12:51:01 -0700

    modified:   auth.py

--- auth.py
+++ auth.py
@@ -72,7 +72,7 @@ def auth_required(f):
                 use_token = False
                 return f(*args, **kwargs)
             else:
-                return make_response("<h1>TO USER YOU DO NOT HAVE PERMISSION TO VIEW THIS DATA PLEASE TRY AGAIN LATER</h1>", 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+                return make_response("<h1>TO USER YOU DO NOT HAVE PERMISSION TO VIEW THIS DATA PLEASE TRY AGAIN LATER</h1>", 401, {'WWW-Authenticate': 'Basic realm="PLEASE LOGIN TO ORDINANCE BREAK"'})
         elif key_header:
             ord_key = db.execute('SELECT * FROM tokens WHERE token = ?', (key_header,)).fetchone()
             if ord_key:
@@ -81,7 +81,7 @@ def auth_required(f):
             else:
                return make_response("<h1>INVALID KEY</h1>", 401)  
         else:
-            return make_response("<h1>TO USER YOU DO NOT HAVE PERMISSION TO VIEW THIS DATA PLEASE TRY AGAIN LATER</h1>", 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+            return make_response("<h1>TO USER YOU DO NOT HAVE PERMISSION TO VIEW THIS DATA PLEASE TRY AGAIN LATER</h1>", 401, {'WWW-Authenticate': 'Basic realm="PLEASE LOGIN TO ORDINANCE BREAK"'})
     return logon
             
             
