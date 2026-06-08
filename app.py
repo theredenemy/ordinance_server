17c5c9f5a090dd481916f33a6d3ddc10c3cbc532
theredenemy <1234corbin.1234ad@gmail.com>
2026-06-07 17:01:17 -0700

    modified:   app.py

--- app.py
+++ app.py
@@ -461,6 +461,6 @@ def ord_render():
     return jsonify({'message': "RENDER"}), 200
 if __name__ == '__main__':
     
-    
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("SERVER_PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
